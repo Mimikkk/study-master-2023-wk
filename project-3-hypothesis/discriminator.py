de7f1d6955d66cb4e6b6b1f1c6394d367b0065ea
Hououin Kyouma <inwazzin@gmail.com>
2023-05-31 22:11:20 +0200

    discr

--- project-3-hypothesis/discriminator.py
+++ project-3-hypothesis/discriminator.py
@@ -2,26 +2,26 @@ from torch import Tensor
 from torch.nn import Sequential, Conv2d, LeakyReLU, BatchNorm2d, Sigmoid, Module, Flatten
 
 class Discriminator(Module):
-  def __init__(self, *, use_gpu: bool):
+  def __init__(self, *, use_gpu: bool, scale: int):
     super(Discriminator, self).__init__()
     self.ngpu = use_gpu and 1 or 0
 
     self.main = Sequential(
-    Conv2d(3, 64, kernel_size=4, stride=2, padding=1, bias=False),
-    BatchNorm2d(64),
-    LeakyReLU(0.2, inplace=True),
-    Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
-    BatchNorm2d(128),
-    LeakyReLU(0.2, inplace=True),
-    Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
-    BatchNorm2d(256),
-    LeakyReLU(0.2, inplace=True),
-    Conv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=False),
-    BatchNorm2d(512),
-    LeakyReLU(0.2, inplace=True),
-    Conv2d(512, 1, kernel_size=4, stride=1, padding=0, bias=False),
-    Flatten(),
-    Sigmoid()
+      Conv2d(3, scale, kernel_size=4, stride=2, padding=1, bias=False),
+      BatchNorm2d(scale),
+      LeakyReLU(0.2, inplace=True),
+      Conv2d(scale, scale * 2, kernel_size=4, stride=2, padding=1, bias=False),
+      BatchNorm2d(scale * 2),
+      LeakyReLU(0.2, inplace=True),
+      Conv2d(scale * 2, scale * 4, kernel_size=4, stride=2, padding=1, bias=False),
+      BatchNorm2d(scale * 4),
+      LeakyReLU(0.2, inplace=True),
+      Conv2d(scale * 4, scale * 8, kernel_size=4, stride=2, padding=1, bias=False),
+      BatchNorm2d(scale * 8),
+      LeakyReLU(0.2, inplace=True),
+      Conv2d(scale * 8, 1, kernel_size=4, stride=1, padding=0, bias=False),
+      Flatten(),
+      Sigmoid()
     )
 
   def forward(self, input: Tensor):
--- project-3-hypothesis/generator.py
+++ project-3-hypothesis/generator.py
@@ -2,24 +2,24 @@ from torch import Tensor
 from torch.nn import Module, Sequential, ConvTranspose2d, BatchNorm2d, ReLU, Tanh
 
 class Generator(Module):
-  def __init__(self, *, use_gpu: bool):
+  def __init__(self, *, use_gpu: bool, scale):
     super(Generator, self).__init__()
     self.ngpu = use_gpu and 1 or 0
 
     self.model = Sequential(
-      ConvTranspose2d(128, 512, kernel_size=4, stride=1, padding=0, bias=False),
-      BatchNorm2d(512),
+      ConvTranspose2d(128, scale * 8, kernel_size=4, stride=1, padding=0, bias=False),
+      BatchNorm2d(scale * 8),
       ReLU(True),
-      ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=False),
-      BatchNorm2d(256),
+      ConvTranspose2d(scale * 8, scale * 4, kernel_size=4, stride=2, padding=1, bias=False),
+      BatchNorm2d(scale * 4),
       ReLU(True),
-      ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1, bias=False),
-      BatchNorm2d(128),
+      ConvTranspose2d(scale * 4, scale * 2, kernel_size=4, stride=2, padding=1, bias=False),
+      BatchNorm2d(scale * 2),
       ReLU(True),
-      ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, bias=False),
-      BatchNorm2d(64),
+      ConvTranspose2d(scale * 2, scale, kernel_size=4, stride=2, padding=1, bias=False),
+      BatchNorm2d(scale),
       ReLU(True),
-      ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1, bias=False),
+      ConvTranspose2d(scale, 3, kernel_size=4, stride=2, padding=1, bias=False),
       Tanh()
     )
 
